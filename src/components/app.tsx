f523ad3df31735a3f61286b02054e5fe9bab6d27
yura4350 <yura435012@gmail.com>
2024-11-22 15:23:26 -0500

    React Fundamentals

---
+++ src/components/app.tsx
@@ -0,0 +1,13 @@
+import Header from "./header"
+
+import {useState, useEffect} from "react"
+
+const App = () => {
+    return (
+        <div>
+            <Header message="Naming contests" />
+        </div>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import {createRoot} from "react-dom/client"
 
-const App = () => {
-    return <div>Hello React</div>;
-}
+import App from "./components/app"
 
 const container = document.getElementById("app")
 const root = createRoot(container)
+
+root.render(<App />);
