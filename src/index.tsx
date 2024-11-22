5e51317a031bbe3d158a61b72cbbec4a4d6a3ee7
yura4350 <yura435012@gmail.com>
2024-11-22 14:18:57 -0500

    Finished Getting Started chapter

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import {createRoot} from "react-dom/client"
+
+const App = () => {
+    return <div>Hello React</div>;
+}
+
+const container = document.getElementById("app")
+const root = createRoot(container)
